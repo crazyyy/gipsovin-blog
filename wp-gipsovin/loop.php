9bf71a5253df43cc13e2661df9eb5b72cf2bce57
Vitaly A <crazyyy@gmail.com>
2015-01-20 08:51:47 +0200

    work done

--- wp-gipsovin/loop.php
+++ wp-gipsovin/loop.php
@@ -1,33 +1,30 @@
+<div class="loop-block clearfix">
+	
 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
 	
 	<!-- article -->
 	<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix looper'); ?>>
-	
-		<!-- post thumbnail -->
-		<a rel="nofollow" class="feature-img" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-			<?php if ( has_post_thumbnail()) :
-				the_post_thumbnail('medium');	
-			else: ?>
-			<img src="<?php echo catchFirstImage(); ?>" title="<?php the_title(); ?>" alt="<?php the_title(); ?>" />
-		<?php endif; ?></a>
-		<!-- /post thumbnail -->
-		
+
 		<!-- post title -->
 		<h2 class="looper-title inner-title">
 			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
 		</h2>
 		<!-- /post title -->
-		
-		<!-- post details -->
-		<span class="date"><?php the_time('j F Y'); ?> <span><?php the_time('G:i'); ?></span></span>
-		<span class="author"><?php _e( 'Published by', 'wpeasy' ); ?> <?php the_author_posts_link(); ?></span>
-		<span class="comments"><?php comments_popup_link( __( 'Leave your thoughts', 'wpeasy' ), __( '1 Comment', 'wpeasy' ), __( '% Comments', 'wpeasy' )); ?></span>
-		<!-- /post details -->
-		
+
 		<?php wpeExcerpt('wpeExcerpt40'); ?>
 		
-		<?php edit_post_link(); ?>
-		
+		<!-- post thumbnail -->
+		<span rel="nofollow" class="feature-img" title="<?php the_title(); ?>">
+			<?php if ( has_post_thumbnail()) :
+				the_post_thumbnail('medium');	
+			else: ?>
+			<img src="<?php echo catchFirstImage(); ?>" title="<?php the_title(); ?>" alt="<?php the_title(); ?>" />
+			<?php endif; ?>
+			<a href="<?php the_permalink(); ?>" class="show-more">Читать</a>
+		</span>
+		<!-- /post thumbnail -->
+		<span class="tags"><?php the_tags( '', ' ', '<br />' ); ?></span>
+		<span class="date"><?php the_time('d F Y'); ?></span>
 	</article>
 	<!-- /article -->
 	
@@ -40,3 +37,6 @@
 	<!-- /article -->
 
 <?php endif; ?>
+
+</div>
+<!-- /.loop-block -->
